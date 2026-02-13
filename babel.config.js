868ac0ccf563a28cdae680cfe1e39c8e3eb38ede
dition0221 <dition0221dev@gmail.com>
2026-02-14 01:49:57 +0900

    chore: setup Expo Router project with TypeScript, Yarn, and Biome

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = (api) => {
+  api.cache(true)
+
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      require.resolve('expo-router/babel'),
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './src',
+          },
+        },
+      ],
+    ],
+  }
+}
